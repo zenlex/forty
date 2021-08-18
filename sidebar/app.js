b2d4c79ec7d9fe1f8c3042e45db2ae8ea4ef4446
Erich Keil <39918654+zenlex@users.noreply.github.com>
2021-08-17 19:32:42 -0500

    Revert to toggle method

--- sidebar/app.js
+++ sidebar/app.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 toggleBtn.addEventListener("click", () =>
-  sidebar.classList.add("show-sidebar")
+  sidebar.classList.toggle("show-sidebar")
 );
 
 closeBtn.addEventListener("click", () =>
