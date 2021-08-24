88fbfd36910299a6716df4fb876d99582d8b0637
Erich Keil <erich@zenlexlife.com>
2021-08-24 18:17:01 -0500

    MVP Complete

--- countdown/app.js
+++ countdown/app.js
@@ -26,31 +26,58 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const digits = document.querySelectorAll('.deadline-format h4');
 
-let endDate = new Date(2021, 11, 23, 11, 17);
+let tempDate = new Date();
+tempDate.setDate(tempDate.getDate() + 10);
+const endDate = tempDate;
+tempDate = null;
 
-//format deadline
-const year = endDate.getFullYear();
-const month = months[endDate.getMonth()];
-const day = weekdays[endDate.getDay()];
-const date = endDate.getDate();
-const hours = endDate.getHours();
-const mins = endDate.getMinutes();
-const secs = endDate.getSeconds();
+//format endDate
+const endYear = endDate.getFullYear();
+const endMonth = months[endDate.getMonth()];
+const endDay = weekdays[endDate.getDay()];
+const endDateNum = endDate.getDate();
+let endHours = endDate.getHours();
+let endMins = endDate.getMinutes();
+let endSecs = endDate.getSeconds();
 
-giveaway.textContent = `${day} ${month} ${date}, ${year}`
+//pad leading 0s
+endHours = endHours < 10 ? '0' + endHours : endHours;
+endMins = endMins < 10 ? '0' + endMins : endMins;
+
+//display endDate
+giveaway.textContent = `giveaway ends ${endDay} ${endMonth} ${endDateNum}, ${endYear} @ ${endHours}:${endMins}`
 
 //calculate remaining time
 function updateCountdown() {
-  let now = new Date();
+  //values in ms
+  const DAYMS = 1000 * 60 * 60 * 24;
+  const HOURMS = 1000 * 60 * 60;
+  const MINMS = 1000 * 60;
+  const SECMS = 1000
+
+  //get distance in ms to deadline from current time
+  let diffms = endDate - Date.now();
+
   //calculate remaining days/hours/mins/secs from now to endDate
-  let remdays = 1;
-  let remhours = 2;
-  let remmins = 3;
-  let remsecs = 4;
-  digits[0].textContent = remdays
-  digits[1].textContent = remhours
-  digits[2].textContent = remmins
-  digits[3].textContent = remsecs
+  let remdays = Math.floor(diffms / DAYMS);
+  let remhours = Math.floor((diffms % DAYMS) / HOURMS);
+  let remmins = Math.floor((diffms % HOURMS) / MINMS);
+  let remsecs = Math.floor((diffms % MINMS) / SECMS);
+
+  //display result
+  const values = [remdays, remhours, remmins, remsecs];
+
+  function preZero(item) {
+    return item = item < 10 ? `0${item}` : item;
+  }
+  digits.forEach((item, index) => item.textContent = preZero(values[index]));
+
+  if (diffms <= 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired</h4>`
+    digits.forEach(item => item.textContent = '00');
+  }
 }
 
-setInterval(updateCountdown, 1000);
+let countdown = setInterval(updateCountdown, 1000);
+updateCountdown();
