3d27929dd4acc7fc8425337dde15b2f4b75e7924
Erich Keil <erich@zenlexlife.com>
2021-09-23 18:54:32 -0500

    Accordion MVP

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about Login</h3>
+        <section className="info">
+          {questions.map((obj, index) => {
+            return <SingleQuestion key={obj.id} {...obj} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setExpanded(!expanded)}>
+          {expanded ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {expanded && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
