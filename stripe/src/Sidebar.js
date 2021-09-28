3df5ce81d5c2cd5376beea43ba2a044c96aace1c
Erich Keil <erich@zenlexlife.com>
2021-09-28 08:54:20 -0500

    Mobile sidebar structure complete
    -Add page headings and sublinks with dummy urls
    -Set sidebar state to closed and hidden at init

--- stripe/src/Sidebar.js
+++ stripe/src/Sidebar.js
@@ -1,3 +1,4 @@
+import { getByLabelText } from "@testing-library/dom";
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 import { useGlobalContext } from "./context";
@@ -15,6 +16,24 @@ const Sidebar = () => {
         <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
+        <div className="sidebar-sublinks">
+          {sublinks.map((obj, index) => {
+            return (
+              <article key={index}>
+                <h4>{obj.page}</h4>
+                {obj.links.map((link, index) => {
+                  const { label, icon, url } = link;
+                  return (
+                    <a key={index} href={url}>
+                      {icon}
+                      {label}
+                    </a>
+                  );
+                })}
+              </article>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
